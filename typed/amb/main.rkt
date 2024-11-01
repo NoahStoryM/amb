782a0a7f64bb45b52663927be2d1f4090d923cfa
NoahStoryM <noahstorym@gmail.com>
2024-11-02 01:34:45 +0800

    Use `→` instead of `->`

--- typed/amb/main.rkt
+++ typed/amb/main.rkt
@@ -10,14 +10,14 @@
   [#:struct (exn:fail:contract:amb exn:fail:contract) ()])
 
 (unsafe-require/typed/provide "../../amb/private/utils.rkt"
-  [current-amb-shuffler (Parameter (∀ (A) (-> (Listof A) (Listof A))))]
+  [current-amb-shuffler (Parameter (∀ (A) (→ (Listof A) (Listof A))))]
   [current-amb-queue    (Parameter (Queue AMB-Task AMB-Task))]
-  [current-amb-enqueue! (Parameter (-> (Queue AMB-Task Any) AMB-Task Void))]
-  [current-amb-dequeue! (Parameter (-> (Queue Nothing AMB-Task) AMB-Task))]
-  [schedule-amb-tasks! (∀ (A ...) (-> (-> A ... A Nothing) (Listof (-> (Values A ... A))) Void))])
+  [current-amb-enqueue! (Parameter (→ (Queue AMB-Task Any) AMB-Task Void))]
+  [current-amb-dequeue! (Parameter (→ (Queue Nothing AMB-Task) AMB-Task))]
+  [schedule-amb-tasks! (∀ (A ...) (→ (→ A ... A Nothing) (Listof (→ (Values A ... A))) Void))])
 
 
-(define-type AMB-Task (->* () ((∀ (A ...) (-> A ... A Nothing))) Nothing))
+(define-type AMB-Task (→* () ((∀ (A ...) (→ A ... A Nothing))) Nothing))
 
 (define-syntax amb
   (λ (stx)
@@ -29,7 +29,7 @@
       [(_ : t) #'(ann (amb) t)]
       [(_ : t alt ...+)
        #'(let/cc k : t
-           (: alt* (Listof (-> t)))
+           (: alt* (Listof (→ t)))
            (define alt* (list (λ () alt) ...))
            (schedule-amb-tasks! k alt*)
            (amb : t))]
@@ -55,11 +55,11 @@
           #:datum-literals (:)
           [(_ : t1 (clause ...) : t2 break:break-clause ... body ...+)
            #`(#,derived-stx
-              : (Listof (-> t1))
+              : (Listof (→ t1))
               (clause ...)
-              : (Listof (-> t2))
+              : (Listof (→ t2))
               break ...
-              (ann (ann (λ () body ...) (-> t2)) (-> t1)))]
+              (ann (ann (λ () body ...) (→ t2)) (→ t1)))]
           [(~or* (name : t0 (clause ...) break:break-clause ... body ...+)
                  (name (clause ...) : t0 break:break-clause ... body ...+)
                  (name (clause ...) break:break-clause ... body ...+))
@@ -83,7 +83,7 @@
              [(attribute t2) #'t2]
              [else #'AnyValues])
            #`(let/cc k : t
-               #;(: alt* (Listof (-> t)))
+               #;(: alt* (Listof (→ t)))
                (define alt* #,((alt*-parser derived-stx) stx))
                (schedule-amb-tasks! k alt*)
                (amb : t))])))
--- typed/data/queue/main.rkt
+++ typed/data/queue/main.rkt
@@ -6,26 +6,26 @@
 (define-type (Queue S T) (Queueof S T)) ; avoid printing #(struct:Queueof ...)
 (define-type QueueTop (Queue Nothing Any))
 (define-type QueueBot (Queue Any Nothing))
-;; TODO (Queue T) -> (Queue T T)
+;; TODO (Queue T) → (Queue T T)
 #;(struct (A ...) _ ([_ : (Parameter A ...)]) #:type-name Queue) ; not work well
 (provide Queue QueueTop QueueBot)
 
 (unsafe-require/typed/provide
  data/queue
- [make-queue (∀ (S T) (-> (Queue S T)))]
+ [make-queue (∀ (S T) (→ (Queue S T)))]
 
- [enqueue! (∀ (S) (-> (Queue S Any) S Void))]
- [enqueue-front! (∀ (S) (-> (Queue S Any) S Void))]
- [dequeue! (∀ (T) (-> (Queue Nothing T) T))]
+ [enqueue! (∀ (S) (→ (Queue S Any) S Void))]
+ [enqueue-front! (∀ (S) (→ (Queue S Any) S Void))]
+ [dequeue! (∀ (T) (→ (Queue Nothing T) T))]
 
- [queue->list (∀ (T) (-> (Queue Nothing T) (Listof T)))]
- [queue-length (-> QueueTop Index)]
+ [queue->list (∀ (T) (→ (Queue Nothing T) (Listof T)))]
+ [queue-length (→ QueueTop Index)]
 
  [queue? (pred QueueTop)]
- [queue-empty? (-> QueueTop Boolean)]
+ [queue-empty? (→ QueueTop Boolean)]
  [non-empty-queue? (∀ (S T)
-                      (case->
-                       (-> (Queue S T) Boolean)
-                       (-> Any Boolean : #:+ QueueTop)))]
+                      (case→
+                       (→ (Queue S T) Boolean)
+                       (→ Any Boolean : #:+ QueueTop)))]
 
- [in-queue (∀ (T) (-> (Queue Nothing T) (Sequenceof T)))])
+ [in-queue (∀ (T) (→ (Queue Nothing T) (Sequenceof T)))])
