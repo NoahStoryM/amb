4a0b31babafdefb315db718212085822df23498d
NoahStoryM <noahstorym@gmail.com>
2024-11-05 10:36:40 +0800

    Update

--- amb/private/utils.rkt
+++ amb/private/utils.rkt
@@ -1,18 +1,21 @@
 #lang racket/base
 
 (require data/queue)
-(provide (all-defined-out))
+(provide (all-defined-out)
+         (rename-out [call-with-no-continuation call/nc]))
 
 
 (struct exn:fail:contract:amb exn:fail:contract ()
   #:extra-constructor-name make-exn:fail:contract:amb
   #:transparent)
 
+(define call-with-no-continuation (λ (f) (f)))
+
 (define current-amb-shuffler (make-parameter reverse))
 (define current-amb-queue    (make-parameter (make-queue)))
 (define current-amb-enqueue! (make-parameter enqueue-front!))
 (define current-amb-dequeue! (make-parameter dequeue!))
-(define current-amb-call     (make-parameter (λ (f) (f))))
+(define current-amb-call     (make-parameter call-with-no-continuation))
 
 (define schedule-amb-tasks!
   (λ (k alt*)
