4ac057465a907880235320e7bcc14edd93e3a09f
NoahStoryM <noahstorym@gmail.com>
2024-11-21 13:30:01 +0800

    Update

--- amb/amb/private/utils.rkt
+++ amb/amb/private/utils.rkt
@@ -19,11 +19,10 @@
 (define current-amb-enqueue! (make-parameter enqueue-front!))
 (define current-amb-dequeue! (make-parameter dequeue!))
 
-(define schedule-amb-tasks!
-  (λ (alt* k)
-    (unless (null? alt*)
-      (define amb-queue    (current-amb-queue))
-      (define amb-enqueue! (current-amb-enqueue!))
-      (for ([alt (in-list ((current-amb-shuffler) alt*))])
-        (define (amb-task) (call-in-continuation k alt))
-        (amb-enqueue! amb-queue amb-task)))))
+(define (schedule-amb-tasks! alt* k)
+  (unless (null? alt*)
+    (define amb-queue    (current-amb-queue))
+    (define amb-enqueue! (current-amb-enqueue!))
+    (for ([alt (in-list ((current-amb-shuffler) alt*))])
+      (define (amb-task) (call-in-continuation k alt))
+      (amb-enqueue! amb-queue amb-task))))
