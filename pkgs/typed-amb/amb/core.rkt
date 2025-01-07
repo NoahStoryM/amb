c011b4f2202476f57252f1fb09af6fac31183f83
NoahStoryM <noahstorym@gmail.com>
2025-01-07 10:57:54 +0800

    [typed-amb] Fix the type of `current-amb-maker`

--- pkgs/typed-amb/amb/core.rkt
+++ pkgs/typed-amb/amb/core.rkt
@@ -16,7 +16,7 @@
   [in-amb*₁ (∀ (a ...) (→ (→ (Values a ... a)) (Sequenceof a ... a)))]
   [current-amb-empty-handler (Parameter (→ Nothing))]
   [current-amb-shuffler (Parameter (∀ (a) (→ (Mutable-Vectorof a) Void)))]
-  [current-amb-maker    (Parameter (→ (→ Nothing) * (Sequenceof (→ Nothing))))]
+  [current-amb-maker    (Parameter (→ (Sequenceof (→ Nothing))))]
   [current-amb-tasks    (Parameter (Sequenceof (→ Nothing)))]
   [current-amb-length   (Parameter (→ SequenceTop Index))]
   [current-amb-pusher   (Parameter (→ (Sequenceof (→ Nothing)) (→ Nothing) Void))]
